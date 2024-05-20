07cc0bb248f43b2b0981c399ccfce66e6943ffc7
Nandini Sharma <114645836+Nun-Thee-Knee@users.noreply.github.com>
2024-05-20 20:18:11 +0530

    Read the torrent file

--- hello.go
+++ hello.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Printf("This is another check")
+	file, err := os.Open("puppy.torrent")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
 }
